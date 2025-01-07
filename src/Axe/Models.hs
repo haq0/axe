e6628141ea6f415d862753ba12e4967a77d3d451
haq <haq@haquire.dev>
2025-01-07 16:17:34 -0500

    Updated to have various methods to handle the parsing (encoding, decoding) of files to generate our models

--- src/Axe/Models.hs
+++ src/Axe/Models.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Axe.Models
-  ( FileList
+  ( FileList(..)
+  , serializeFromFile
   ) where
 
 import Control.Monad (mzero)
 import Data.Aeson
-import Data.Text
+import qualified Data.ByteString.Lazy as LB
+import Data.Text hiding (filter)
 
 data FileList = FileList
   { name :: Text
@@ -24,3 +27,27 @@ instance FromJSON FileList where
       <$> v .: "name"
       <*> v .: "files"
   parseJSON _ = mzero
+
+serializeFromFile ::
+     FilePath -> IO [FileList]
+serializeFromFile path =
+  decodeFileStrict path >>= \case
+    Just fileList -> return fileList
+    Nothing ->
+      error "Failed to parse JSON file"
+
+removeItem ::
+     Text -> [FileList] -> [FileList]
+removeItem a b =
+  filter (\b -> name b /= a) b
+
+addItem ::
+     FileList
+  -> [FileList]
+  -> [FileList]
+addItem new list = new : list
+
+writeFileLists ::
+     FilePath -> [FileList] -> IO ()
+writeFileLists path fileLists =
+  LB.writeFile path $ encode fileLists
